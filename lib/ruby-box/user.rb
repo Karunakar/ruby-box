3229d2fd4242ad945563a60ae69f30f3aa443a8a
Your Name Comes Here <me@here.com>
2013-10-15 23:55:04 +0530

    adding a new method to retrive all users from the dropbox account

--- lib/ruby-box/user.rb
+++ lib/ruby-box/user.rb
@@ -1,11 +1,18 @@
 module RubyBox
   class User < Item
+    def initialize(session)
+      @session = session
+    end
     
     def enterprise
       resp = @session.get( "#{RubyBox::API_URL}/users/#{id}?fields=enterprise" )
       resp["enterprise"]
     end
-    
+
+    def user_list
+     	 resp = @session.get( "#{RubyBox::API_URL}/users" )
+    end 
+
     private
 
     def resource_name
